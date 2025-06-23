17591014b684254565221f8b3f13c1cd7f0a4e39
Huu Van Hoang <vanhuu.0924@gmail.com>
2025-06-23 19:34:15 +0700

    fix build

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-    dirs: ["src"],
-  },
+  // trailingSlash: true,
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  //   dirs: ["src"],
+  // },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
